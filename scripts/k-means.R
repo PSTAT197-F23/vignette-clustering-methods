52b80d0061350c5b0ba77133025f53fb54d0cbf1
jadethai <91998047+jadethai@users.noreply.github.com>
2023-11-29 16:16:43 -0800

    Update k-means.R

--- scripts/k-means.R
+++ scripts/k-means.R
@@ -1,4 +1,12 @@
+# load packages
+library(tidyverse)
 
+# read in data
 data <- read.csv('data/winequalityN.csv', header = TRUE)
-head(data)
 
+# separate predictors from response (we do not need the response variable here)
+x <- select(data, -quality)
+
+# true cluster plot
+ggplot(data, aes(alcohol, pH, color = factor(quality))) +
+  geom_point()
