62076d25b587d8094417d2d31b4c37221ede6792
manuri-alwis <manuri_alwis@ucsb.edu>
2023-11-29 22:11:17 -0800

    update reading in data

--- scripts/k-means.R
+++ scripts/k-means.R
@@ -2,7 +2,11 @@
 library(tidyverse)
 
 # read in data
-data <- read.csv('data/winequalityN.csv', header = TRUE)
+root_dir <- rprojroot::find_rstudio_root_file()
+data_dir <- file.path(root_dir, "data")
+setwd(data_dir)
+data <- read.csv("winequalityN.csv", header = TRUE)
+#data <- read.csv('data/winequalityN.csv', header = TRUE)
 
 # separate predictors from response (we do not need the response variable here)
 x <- select(data, -quality)
