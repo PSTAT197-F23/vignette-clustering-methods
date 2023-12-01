89cff4c2e15328c262338030b6093d03e5d78d59
jenrink <jennifererink@gmail.com>
2023-12-01 11:02:20 -0800

    found out that divisive hierarchical clustering might not work with our data?

--- scripts/jenrink-script.R
+++ scripts/jenrink-script.R
@@ -13,11 +13,12 @@ root_dir <- rprojroot::find_rstudio_root_file()
 data_dir <- file.path(root_dir, "data")
 setwd(data_dir)
 
-# 7 different types of quality : "6 5 7 8 4 3 9"
-unique(wines$quality)
 
 wines <- read_csv('winequalityN.csv')
 summary(wines)
+
+# 7 different types of quality : "6 5 7 8 4 3 9"
+unique(wines$quality)
 
 # Remove variable `quality` because we want to do clustering for quality
 wines_removed <- subset(wines, select = -c(type, quality))
@@ -59,3 +60,20 @@ table(clusterCut_euc, wines$quality)
 # Plot the clusters
 ggplot(wines, aes(`fixed acidity`, alcohol, color = wines$quality)) + 
   geom_point(alpha = 0.4, size = 1) + geom_point(col = clusterCut_euc)
+
+
+
+# Agglomerative Hierarchical Clustering
+# We can perform agglomerative HC with hclust. 
+# First we compute the dissimilarity values with dist and then feed these values into hclust and specify the agglomeration method to be used (i.e. “complete”, “average”, “single”, “ward.D”). We can then plot the dendrogram.
+
+# Dissimilarity matrix
+agg_dist <- dist(wine_norm, method = "euclidean")
+# Hierarchical clustering using Complete Linkage
+hc1 <- hclust(agg_dist, method = "complete" )
+# Plot the obtained dendrogram
+plot(hc1, cex = 0.6, hang = -1)
+
+
+# Divisive Hierarchical Clustering
+# Won't work for our data set? Because we have so many observations
