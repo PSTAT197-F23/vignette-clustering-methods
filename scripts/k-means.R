22001513fad61feb69d27c28a8c6145cc11188d0
kayla-katakis <kaylakatakis50@yahoo.com>
2023-11-30 17:26:20 -0800

    true cluster for white wine

--- scripts/k-means.R
+++ scripts/k-means.R
@@ -5,12 +5,14 @@ library(tidyverse)
 root_dir <- rprojroot::find_rstudio_root_file()
 data_dir <- file.path(root_dir, "data")
 setwd(data_dir)
-data <- read.csv("winequalityN.csv", header = TRUE)
+#data <- read.csv("winequalityN.csv", header = TRUE)
 #data <- read.csv('data/winequalityN.csv', header = TRUE)
 
+data <- read_csv('white.csv')
 # separate predictors from response (we do not need the response variable here)
 x <- select(data, -quality)
 
 # true cluster plot
 ggplot(data, aes(alcohol, pH, color = factor(quality))) +
   geom_point()
+#clusters iffy
