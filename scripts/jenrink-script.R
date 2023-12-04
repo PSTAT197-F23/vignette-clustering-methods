074a3be2e3ace552262ac72e7a01f01a0c86dc5b
jenrink <jennifererink@gmail.com>
2023-12-03 20:28:16 -0800

    finished code?

--- scripts/jenrink-script.R
+++ scripts/jenrink-script.R
@@ -2,6 +2,8 @@
 
 #kayla was here
 
+# try Random Samples!!
+
 library(MASS)
 library(class)
 library(tidyverse)
@@ -9,6 +11,8 @@ library(ISLR)
 library(ISLR2)
 library(factoextra)
 library(cluster)
+library(ggplot2)
+library(hrbrthemes)
 root_dir <- rprojroot::find_rstudio_root_file()
 data_dir <- file.path(root_dir, "data")
 setwd(data_dir)
@@ -17,63 +21,58 @@ setwd(data_dir)
 housing <- read_csv('housing.csv')
 summary(housing)
 
-# 7 different types of quality : "6 5 7 8 4 3 9"
-unique(wines$quality)
 
 # Remove variable `quality` because we want to do clustering for quality
-wines_removed <- subset(wines, select = -c(type, quality))
-summary(wines_removed)
+#wines_removed <- subset(wines, select = -c(type, quality))
+#summary(wines_removed)
+
+# Pick just MedInc, Population, Latitude, and Longitude
+subset_housing <- select(housing, MedInc, Population, Latitude, Longitude)
 
 # Normalize Data
-normalization<-function(x){
-  return((x-mean(x))/sd(x))
-}
+housing_norm<-as.data.frame(scale(subset_housing))
 
-wine_norm<-wines_removed %>%
-  mutate(fixed.acidity=normalization(`fixed acidity`),
-         volatile.acidity=normalization(`volatile acidity`),
-         citric.acid=normalization(`citric acid`),
-         residual.sugar=normalization(`residual sugar`),
-         chlorides=normalization(chlorides),
-         free.sulfur.dioxide=normalization(`free sulfur dioxide`),
-         total.sulfur.dioxide=normalization(`total sulfur dioxide`),
-         density=normalization(density),
-         pH=normalization(pH),
-         sulphates=normalization(sulphates),
-         alcohol=normalization(alcohol))
-
-summary(wine_norm)
+# Graph of California
+ggplot(housing_norm, aes(x=Latitude, y=Longitude, color=MedInc)) + 
+  geom_point(size=1) +
+  theme_ipsum()
 
 
-# Simple Hierarchy
-clusters <- hclust(dist(wine_norm))
-plot(clusters)
-clusterCut <- cutree(clusters, 7)
-table(clusterCut, wines$quality)
+# Ward Hierarchical Clustering
+d <- dist(housing_norm,
+          method = "euclidean") # distance matrix
+fit <- hclust(d, method="ward.D2")
 
-# Euclidean Distance (better performance than simple)
-clusters_euc <- hclust(dist(wine_norm), method = 'average')
-plot(clusters_euc)
-clusterCut_euc <- cutree(clusters_euc, 7)
-table(clusterCut_euc, wines$quality)
+plot(fit, cex=0.000001) # display dendogram
+rect.hclust(fit, k=8)
 
-# Plot the clusters
-ggplot(wines, aes(`fixed acidity`, alcohol, color = wines$quality)) + 
-  geom_point(alpha = 0.4, size = 1) + geom_point(col = clusterCut_euc)
+groups <- cutree(fit, k=5)
 
+library(cluster)
+clusplot(housing_norm, groups, color=TRUE, shade=TRUE, labels=2, lines=0)
 
+library(fpc)
+clusters <- 5
+clus.boot <- clusterboot(housing_norm, 
+                         B=50, # Number of bootstrap resamples
+                         clustermethod=hclustCBI, # for hierarchical clustering 
+                         method="ward.D2", # use what we used in "hclust"
+                         k=clusters, 
+                         count=FALSE) # Show progress on screen?
+# clus.boot
 
-# Agglomerative Hierarchical Clustering
-# We can perform agglomerative HC with hclust. 
-# First we compute the dissimilarity values with dist and then feed these values into hclust and specify the agglomeration method to be used (i.e. “complete”, “average”, “single”, “ward.D”). We can then plot the dendrogram.
+# Rule of thumb: . AvgJaccard <0.6 is unstable . AvgJaccard >0.85 is highly stable
+set.seed(12032023)
+AvgJaccard <- clus.boot$bootmean
+Instability <- clus.boot$bootbrd/1000
+Clusters <- c(1:clusters)
+Eval <- cbind(Clusters, AvgJaccard, Instability)
+Eval
 
-# Dissimilarity matrix
-agg_dist <- dist(wine_norm, method = "euclidean")
-# Hierarchical clustering using Complete Linkage
-hc1 <- hclust(agg_dist, method = "complete" )
-# Plot the obtained dendrogram
-plot(hc1, cex = 0.6, hang = -1)
+#Clusters AvgJaccard Instability
+#[1,]        1  0.8501335       0.000
+#[2,]        2  0.3051210       0.005
+#[3,]        3  0.7852603       0.000
+#[4,]        4  0.2251899       0.005
+#[5,]        5  0.9000000       0.001
 
-
-# Divisive Hierarchical Clustering
-# Won't work for our data set? Because we have so many observations
