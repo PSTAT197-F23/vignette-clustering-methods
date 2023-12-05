27c62b19c9e60149236c0c199844aa89e73078a0
jenrink <jennifererink@gmail.com>
2023-12-05 10:19:30 -0800

    update scripts

--- scripts/jenrink-script.R
+++ scripts/jenrink-script.R
@@ -1,10 +1,9 @@
-# Hierarchical Clustering Script
+# Jen Rink Script
 
-#root_dir <- rprojroot::find_rstudio_root_file()
-#data_dir <- file.path(root_dir, "data")
-#setwd(data_dir)
+#kayla was here
 
-# Add Libraries
+# try Random Samples!!
+
 library(MASS)
 library(class)
 library(tidyverse)
@@ -13,42 +12,45 @@ library(ISLR2)
 library(factoextra)
 library(cluster)
 library(ggplot2)
+library(hrbrthemes)
+root_dir <- rprojroot::find_rstudio_root_file()
+data_dir <- file.path(root_dir, "data")
+setwd(data_dir)
 
-# read in data
+
 housing <- read_csv('housing.csv')
 summary(housing)
 
-# pick just MedInc, Population, Latitude, and Longitude variables for analysis
+
+# Remove variable `quality` because we want to do clustering for quality
+#wines_removed <- subset(wines, select = -c(type, quality))
+#summary(wines_removed)
+
+# Pick just MedInc, Population, Latitude, and Longitude
 subset_housing <- select(housing, MedInc, Population, Latitude, Longitude)
 
-# normalize Data
+# Normalize Data
 housing_norm<-as.data.frame(scale(subset_housing))
 
-# Plot of California distinguised by Median Income
+# Graph of California
 ggplot(housing_norm, aes(x=Latitude, y=Longitude, color=MedInc)) + 
-  geom_point(size=1)
+  geom_point(size=1) +
+  theme_ipsum()
 
 
-# perform hierarchical clustering using "ward.D2" metric and euclidean distance
+# Ward Hierarchical Clustering
 d <- dist(housing_norm,
           method = "euclidean") # distance matrix
 fit <- hclust(d, method="ward.D2")
 
-# display dendogram
-plot(fit, labels=FALSE)
-
-# display optimal cut off
+plot(fit, labels=FALSE) # display dendogram
 rect.hclust(fit, k=5)
 
-# cut the dendogram
 groups <- cutree(fit, k=5)
 
-# perform additional visualization using clusplot()
 library(cluster)
 clusplot(housing_norm, groups, color=TRUE, shade=TRUE, labels=0, lines=0)
 
-
-# performance analysis using bootstrapping
 library(fpc)
 clusters <- 5
 clus.boot <- clusterboot(housing_norm, 
@@ -58,20 +60,25 @@ clus.boot <- clusterboot(housing_norm,
                          k=clusters, 
                          count=FALSE) # Show progress on screen?
 
-# Save the output object to a file for future use because the function takes awhile to run
+# Save an object to a file
 saveRDS(clus.boot, file = "~/FinalProject_Group4/data/HCLUST_bootstrap.rds")
 # Restore the object
 #readRDS(file = "HCLUST_bootstrap.rds")
 
+# clus.boot
 
-
-# Rule of thumb: . Average Jaccard Index of<0.6 is unstable.
-# Average Jaccard Index of >0.85 is highly stable.
-
-# set seed for reproducibility
+# Rule of thumb: . AvgJaccard <0.6 is unstable . AvgJaccard >0.85 is highly stable
 set.seed(12032023)
 AvgJaccard <- clus.boot$bootmean
 Instability <- clus.boot$bootbrd/1000
 Clusters <- c(1:clusters)
 Eval <- cbind(Clusters, AvgJaccard, Instability)
 Eval
+
+#     Clusters AvgJaccard Instability
+#[1,]        1  0.5160727       0.013
+#[2,]        2  0.5532106       0.024
+#[3,]        3  0.4430299       0.032
+#[4,]        4  0.5287624       0.029
+#[5,]        5  0.7387931       0.002
+
