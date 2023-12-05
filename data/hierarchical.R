d2ec2eade4bbadbf7ed4088b6fa4151d845b231b
jenrink <jennifererink@gmail.com>
2023-12-05 10:14:04 -0800

    create new final script for hierarchical clustering

---
+++ data/hierarchical.R
@@ -0,0 +1,15 @@
+# Hierarchical Clustering Script
+
+#root_dir <- rprojroot::find_rstudio_root_file()
+#data_dir <- file.path(root_dir, "data")
+#setwd(data_dir)
+
+library(MASS)
+library(class)
+library(tidyverse)
+library(ISLR)
+library(ISLR2)
+library(factoextra)
+library(cluster)
+library(ggplot2)
+library(hrbrthemes)
